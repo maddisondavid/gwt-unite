5d652ecb85d12782f72ceed253a5bd2dd4780186
maddisondavid <maddisondavid@e14673ba-a55b-11de-bf1c-ef771b8a44ac>
2009-09-21 13:11:50 +0000

    Initial Import

---
+++ GWTUnite/src/opera/markuper/ExtendedElement.java
@@ -0,0 +1,10 @@
+package opera.markuper;
+
+import com.google.gwt.core.client.JavaScriptObject;
+
+public final class ExtendedElement extends JavaScriptObject {
+
+	protected ExtendedElement() {
+		
+	}
+}
