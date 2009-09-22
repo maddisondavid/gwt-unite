3d3ad6eae5688d6a9e59d3d8da206a98c6607de8
maddisondavid <maddisondavid@e14673ba-a55b-11de-bf1c-ef771b8a44ac>
2009-09-22 07:49:46 +0000

    Fixed certain API methods Added (Some) Javascript comments

--- trunk/GWTUnite/src/opera/io/WebServerConnection.java
+++ trunk/GWTUnite/src/opera/io/WebServerConnection.java
@@ -2,35 +2,64 @@ package opera.io;
 
 import com.google.gwt.core.client.JavaScriptObject;
 
+/** Connection made to the Web server.
+*
+* <p>WebServerConnection holds the incoming request and gives the user access to the outgoing response,
+* as well maintaining the state of the connection.</p>
+*/
 final public class WebServerConnection extends JavaScriptObject {
 	
 	protected WebServerConnection() {
 	}
-	
-	public native boolean getClosed() /*-{
-		return this.closed;
-	}-*/;
 
 	public native int getId() /*-{
 		return this.id;
 	}-*/;
 
+    /**
+     * The incoming HTTP request on this connection.
+     */
 	public native WebServerRequest getRequest() /*-{
 		return this.request;
 	}-*/;
 
+    /**
+     * The outgoing HTTP response that will be send to the client.
+     */
 	public native WebServerResponse getResponse() /*-{
 		return this.response;
 	}-*/;
 
+    /**
+     * Whether or not this connection has been closed.
+     */
+	public native boolean isClosed() /*-{
+		return this.closed;
+	}-*/;
+	
+    /**
+     * Whether or not this connection is made directly through the Opera instance.
+     */
 	public native boolean isLocal() /*-{
 		return this.isLocal;
 	}-*/;
 
+    /**
+     * Whether this connection is made from a page with a URL on the admin subdomain in the same instance running the service.
+     *
+     * <p>You can use this property to determine if the connection is coming from the owner (typically yourself) of the service and
+     * therefore whether to, for example, grant it special privileges. This is the case when the request is the result of accessing a 
+     * URL with the admin subdomain.</p>
+     */
 	public native boolean isOwner() /*-{
 		return this.isOwner;
 	}-*/;
 
+    /**
+     * Whether or not this connection is made through the proxy.
+     *
+     * <p>This property will be false if you access the services through a local URL.</p>
+     */
 	public native boolean isProxied() /*-{
 		return this.isProxied;
 	}-*/;
--- trunk/GWTUnite/src/opera/io/WebServerRequestEvent.java
+++ trunk/GWTUnite/src/opera/io/WebServerRequestEvent.java
@@ -2,14 +2,34 @@ package opera.io;
 
 import com.google.gwt.core.client.JavaScriptObject;
 
+/**
+* Event fired when requests are made to the Web server.
+*
+* <p>See {@link WebServer#addEventListener(String, WebServerEventHandler, boolean)} for details on different event names.</p>
+*/
 final public class WebServerRequestEvent extends JavaScriptObject {
 	protected WebServerRequestEvent() {
 	}
-	
+
+	/**
+	 * The connection this request event was generated from.
+	 *
+	 * <p>Use this property to access the incoming request and outgoing response:</p>
+	 * 
+	 * <pre><code>req = e.getConnection.request;
+	 *res = e.connection.response;</code></pre>
+	 *
+	 * <p>If this event was fired for a <code>_close</code> request, this property will be <code>null</code>.</p>
+	 */
 	native public WebServerConnection getConnection() /*-{
 		return this.connection;
 	}-*/;
 
+	/**
+	 * Id of the connection this request was generated from.
+	 *
+	 * If this event was fired for a <code>_close</code> request, this property will hold the id of the closed connection.
+	 */
 	public native int getId() /*-{
 		return this.id
 	}-*/;
