b2d90d9fa8476db02a64879deb788031244fe0dc
maddisondavid <maddisondavid@e14673ba-a55b-11de-bf1c-ef771b8a44ac>
2009-10-10 21:01:40 +0000

    Deleted GWTUnite-TestBench

--- trunk/GWTUnite-TestBench/src/org/gwtunite/testing/client/framework/Test.java
+++
@@ -1,6 +0,0 @@
-package org.gwtunite.testing.client.framework;
-
-/** Signifies a method is a Test method */
-public @interface Test {
-
-}
--- trunk/GWTUnite-TestBench/src/org/gwtunite/testing/client/framework/TestResultListener.java
+++
@@ -1,8 +0,0 @@
-package org.gwtunite.testing.client.framework;
-
-public interface TestResultListener {
-
-	void onPass();
-	void onFailed(String message);
-	void onError(Exception exception);
-}
