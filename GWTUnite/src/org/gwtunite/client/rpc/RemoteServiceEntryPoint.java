fdb436d442a618d4a95d305600e29b4728cb7784
maddisondavid <maddisondavid@e14673ba-a55b-11de-bf1c-ef771b8a44ac>
2009-10-23 08:52:41 +0000

    Renamed RemoteServiceApplication to RemoteServiceEntryPoint

--- GWTUnite/src/org/gwtunite/client/rpc/RemoteServiceEntryPoint.java
+++ GWTUnite/src/org/gwtunite/client/rpc/RemoteServiceEntryPoint.java
@@ -2,12 +2,13 @@ package org.gwtunite.client.rpc;
 
 import java.util.Map;
 
+import org.gwtunite.client.commons.Logging;
 import org.gwtunite.client.net.OperaUniteEntryPoint;
 import org.gwtunite.client.net.WebServer;
 
 import com.google.gwt.core.client.GWT;
 
-public class RemoteServiceApplication extends OperaUniteEntryPoint {
+public class RemoteServiceEntryPoint extends OperaUniteEntryPoint {
 	@Override
 	public void init(WebServer webServer) {
 		final RemoteServiceRegistry registry = GWT.create(RemoteServiceRegistry.class);
